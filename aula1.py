5d28995d4239a8b053273559518c49bca9ec2378
karlloscosta <karllos.costa@alunos.ufersa.edu.br>
2024-06-04 01:10:31 -0300

    New Commit

---
+++ aula1.py
@@ -0,0 +1,13 @@
+"Classes"
+
+#São moldes para criar novos objeto
+#Convenção de nomenclatura PascalCase
+
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = 'Karllos'
+p1.sobrenome = 'Eduardo'
+
+print(p1.nome,p1.sobrenome)
